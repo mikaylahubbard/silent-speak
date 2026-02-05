3c6c57da8b468251f2e90e5f4d08222fe0f8f6ee
Mikayla <mikayla@thehubbards.org>
2026-02-05 01:34:14 -0500

    begin profile page design

--- app/(drawer)/profile.tsx
+++ app/(drawer)/profile.tsx
@@ -2,19 +2,49 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Profile() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.name}>Mikayla Hubbard</Text>
-      <Text style={styles.email}>mikayla@example.com</Text>
+    <View style={styles.screen}>
+      <View style={styles.header}>
+        <Text style={styles.name}>Mikayla Hubbard</Text>
+        <Text style={styles.email}>mikayla@example.com</Text>
+      </View>
+      <View style={styles.body}>
+        {/* Personal Info */}
+        <View>
+          <Text style={styles.heading}>Personal Info</Text>
+          <View style={styles.line} />;
+          <Text style={styles.text}>Username: </Text>
+          <Text style={styles.text}>Phone Number: </Text>
+          <Text style={styles.text}>Age:</Text>
+          <Text style={styles.text}>Change Password</Text>
+        </View>
+        {/* Settings */}
+        <View>
+          <Text style={styles.heading}>Settings</Text>
+          <View style={styles.line} />;
+          <Text style={styles.text}>Light Mode/Dark mode</Text>
+          <Text style={styles.text}>Custom Theme</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+  },
+  header: {
     alignItems: "center",
     paddingTop: 70,
     backgroundColor: "#fff",
+  },
+  body: {
+    padding: 30,
+  },
+  line: {
+    borderBottomColor: "lightgray",
+    borderBottomWidth: 1,
+    marginVertical: 10,
   },
   name: {
     fontSize: 24,
@@ -24,5 +54,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "gray",
     marginBottom: 40,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginTop: 24,
+  },
+  text: {
+    padding: 10,
   },
 });
