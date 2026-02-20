6cf743e67f700137ccb9b1552843068f9960140b
Mikayla <mikayla@thehubbards.org>
2026-02-20 15:39:32 -0500

    update profile page visuals

--- app/(app)/(drawer)/profile.tsx
+++ app/(app)/(drawer)/profile.tsx
@@ -1,12 +1,12 @@
 import { User } from "firebase/auth";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Switch, Text, View } from "react-native";
 import { auth } from "../../../lib/firebase-config";
-
 export default function Profile() {
   const [user, setUser] = useState<User | null>(null);
-
+  const [isEnabled, setIsEnabled] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const [Enable, setEnable] = useState("courses");
   const [form, setForm] = useState({
     username: "",
     phone: "",
@@ -31,33 +31,53 @@ export default function Profile() {
     return <Text>Loading profile...</Text>;
   } else
     return (
-      <View style={styles.screen}>
-        <View style={styles.header}>
-          <Text style={styles.name}>{user.displayName}</Text>
-          <Text style={styles.email}>{user.email}</Text>
+      <View className="flex-1">
+        <View className="justify-center items-center pt-16 pb-12 bg-neutral-200">
+          <Text className="text-3xl font-bold text-neutral-700 ">
+            {user.displayName}
+          </Text>
+          <Text className="text-lg text-neutral-500 font-light">
+            {user.email}
+          </Text>
         </View>
-        <View style={styles.body}>
+        <View className="p-10">
           {/* Personal Info */}
           <View>
-            <Text style={styles.heading}>Personal Info</Text>
-            <View style={styles.line} />
-            <Text style={styles.text}>Username: {user.displayName}</Text>
+            <Text className="font-bold text-xl text-neutral-800">
+              Personal Info
+            </Text>
+            <View className="border-b border-neutral-300 my-2" />
+            <Text className="p-3">Username: {user.displayName}</Text>
             {user.phoneNumber ? (
-              <Text style={styles.text}>Phone Number: {user.phoneNumber}</Text>
+              <Text className="p-3">Phone Number: {user.phoneNumber}</Text>
             ) : (
-              <Text style={styles.text}>Phone Number: -</Text>
+              <Text className="p-3">Phone Number: -</Text>
             )}
             {/* will need to create firestore in order to display custom info like age */}
-            <Text style={styles.text}>Age: -</Text>
+            <Text className="p-3">Age: -</Text>
 
-            <Text style={styles.text}>Change Password</Text>
+            <Text className="p-3 text-violet-800">Change Password</Text>
           </View>
           {/* Settings */}
           <View>
-            <Text style={styles.heading}>Settings</Text>
-            <View style={styles.line} />
-            <Text style={styles.text}>Light Mode/Dark mode</Text>
-            <Text style={styles.text}>Custom Theme</Text>
+            <Text className="font-bold text-xl text-neutral-800 pt-5">
+              Settings
+            </Text>
+            <View className="border-b border-neutral-300 my-2" />
+            <View className="flex-row items-center justify-between">
+              <Text className="p-3">Dark Mode</Text>
+              {/* NEEDS IMPLEMENTATION */}
+              <Switch
+                trackColor={{ false: "#404040", true: "#5b21b6" }}
+                thumbColor={isEnabled ? "#f5f5f5" : "#f5f5f5"}
+                ios_backgroundColor="#404040"
+                onValueChange={setIsEnabled}
+                value={isEnabled}
+                style={{ transform: [{ scale: 0.7 }] }}
+              />
+            </View>
+            {/* NEEDS IMPLEMENTATION */}
+            <Text className="p-3">Set Highlight color:</Text>
           </View>
         </View>
       </View>
